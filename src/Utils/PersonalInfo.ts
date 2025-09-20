521acf57ff510315b2a06691743c8b07e77ee643
Ibrahim Younes Zaidi <zaidibrahim2003@Gmail.com>
2025-09-20 15:17:55 +0100

    separated static information

---
+++ src/Utils/PersonalInfo.ts
@@ -0,0 +1,11 @@
+const MyPersonalInfo = {
+  name: "Ibrahim Zaidi",
+  profiles: {
+    github: "https://github.com/Ibrahim-Zaidi",
+    linkedin: "https://www.linkedin.com/in/ibrahim-zaidi-512a80243/",
+    twitter: "https://twitter.com/ibrahimzaidi",
+    email: "ibrahim.zaidi@univ-alger.dz",
+  },
+};
+
+export default MyPersonalInfo;
---
+++ src/Utils/TechProvider.ts
@@ -0,0 +1,49 @@
+type Technology = {
+  name: string;
+  icon: string;
+};
+
+const technologies: Technology[] = [
+  {
+    name: "HTML",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/html5/html5-original.svg",
+  },
+  {
+    name: "CSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/css3/css3-original.svg",
+  },
+  {
+    name: "TailwindCSS",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon@latest/icons/tailwindcss/tailwindcss-original.svg",
+  },
+  {
+    name: "JavaScript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/javascript/javascript-original.svg",
+  },
+  {
+    name: "TypeScript",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/typescript/typescript-original.svg",
+  },
+  {
+    name: "React",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/react/react-original.svg",
+  },
+  {
+    name: "Node.js",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/nodejs/nodejs-original.svg",
+  },
+  {
+    name: "Express",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/express/express-original.svg",
+  },
+  {
+    name: "MongoDB",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/mongodb/mongodb-original.svg",
+  },
+  {
+    name: "PostgreSQL",
+    icon: "https://cdn.jsdelivr.net/gh/devicons/devicon/icons/postgresql/postgresql-original.svg",
+  },
+];
+
+export default technologies;
